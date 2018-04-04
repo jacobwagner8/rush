3e5c2ccb0e07878088cffcafc35dbe67ba776db7
Ubuntu <ubuntu@ip-172-31-21-101.us-west-2.compute.internal>
2018-04-04 21:12:05 +0000

    .

--- s3Upload.js
+++ s3Upload.js
@@ -8,9 +8,7 @@ const S3Client = require('s3-browser-direct-upload');
 const dev = process.env.ENV !== 'prod';
 
 var config = require('./config');
-
-if (dev)
-  var secrets = require('./secrets');
+var secrets = require('./secrets');
 
 const s3ClientOptions = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID || secrets.aws_id,
